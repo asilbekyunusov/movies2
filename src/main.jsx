be75d70b8baa7f55d154d649e7d8d82a7921597b
Asilbek <steelyunusov@gmail.com>
2025-06-01 18:35:35 +0500

    CI/CD qilndi

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import './index.css'
 
+// 👇 Service Worker’ni unregister qilish (cache’ni oldini olish)
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(registrations => {
+    for (let registration of registrations) {
+      registration.unregister();
+    }
+  });
+}
+
+// 👇 React ilovani render qilish
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
